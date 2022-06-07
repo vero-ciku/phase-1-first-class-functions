60d705be7086dec746f60d157d43686356f2d048
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-07 16:15:48 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction(callback){
+    callback();
+    }
+    function returnsANamedFunction(){
+        return returnsANamedFunction;
+    }
+    function returnsAnAnonymousFunction()
+    {
+        return function(){}
+    }
